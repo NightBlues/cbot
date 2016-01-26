29a40cca838e469c7043de5d80abc99ab1c799ff
Vadim Radovel <vadim@radovel.ru>
2016-01-26 22:12:43 +0300

    Inited tcp_repairer.

--- src/main.c
+++ src/main.c
@@ -3,16 +3,32 @@
 #include "repairer.c"
 
 
+
 int main() {
   printf("Hello, world!\n");
   timerloop * loop = timerloop_create();
-  repairer * rep = repairer_create("echo \"hello\"", 3);
-  int my_check(repairer * rep) { return 1;}
-  rep->_check_func = my_check;
-  loopswinger(loop, rep);
+  int counter = 10;
+  void loop_limiter(timerloop * loop, int * count) {
+    (*count)--;
+    printf("Called loop limiter, counter = %d\n", *count);
+    if(*count <= 0) {
+      /* loop->stopped = 1; */
+      loop->gracefully_stopped = 1;
+      return;
+    }
+    timerloop_add_timeout(loop, 1, loop_limiter, count);
+  }
+  timerloop_add_timeout(loop, 1, loop_limiter, &counter);
+  struct hosts hosts[2] = {{"ya.ru", "80"}, {"google.com", "80"}};
+  tcp_repairer * rep = tcp_repairer_create(hosts, 2, "echo repairing...", 3);
+
+  loopswinger(loop, (repairer *) rep);
   timerloop_start(loop);
-  timerloop_destroy(loop);
   
+  if(repairer_destroy((repairer *) rep) != 0) {
+    printf("Couldn't free memory for repairer\n");
+  }
+  timerloop_destroy(loop);
   
   return 0;
 }
--- src/repairer.c
+++ src/repairer.c
@@ -18,6 +18,18 @@ struct repairer {
   int (* _check_func)(repairer *);
 };
 
+struct hosts {
+    char hostname[255];
+    char port[6];
+};
+
+typedef struct tcp_repairer tcp_repairer;
+struct tcp_repairer {
+  repairer base;
+  struct hosts * hosts;
+  int hosts_count;
+};
+
 
 int repairer_dumb_check(repairer * rep) {
   return 0;
@@ -35,6 +47,12 @@ repairer * repairer_create(char * repair_command, int poll_interval) {
   rep->_check_func = repairer_dumb_check;
 
   return rep;
+}
+
+
+int repairer_destroy(repairer * rep) {
+  free(rep);
+  return 0;
 }
 
 
@@ -93,4 +111,22 @@ int repairer_check(repairer * rep) {
 void loopswinger(timerloop *, repairer *);
 void loopswinger(timerloop * loop, repairer * rep) {
   timerloop_add_timeout(loop, repairer_check(rep), loopswinger, rep);
+}
+
+
+int tcp_repairer_check(repairer * rep) {
+  tcp_repairer * trep = (tcp_repairer *) rep;
+  printf("We called tcp repairer, hosts_count: %d\n", trep->hosts_count);
+  return 0;
+}
+
+
+tcp_repairer * tcp_repairer_create(struct hosts * hosts, int hosts_count, char * repair_command, int poll_interval) {
+  repairer * base = repairer_create(repair_command, poll_interval);
+  base->_check_func = tcp_repairer_check;
+  tcp_repairer * res = realloc(base, sizeof(tcp_repairer));
+  res->hosts = hosts;
+  res->hosts_count = hosts_count;
+
+  return res;
 }
